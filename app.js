365783cf10a6fff42cf734dec2e27f95f362feea
giuviglia94 <vigliaturo94@gmail.com>
2021-09-24 12:43:12 +0200

    refactoring

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const checkAuth = require('./middleware/user-auth')
+const {verifyToken,signToken,deleteToken} = require('./middleware/user-auth')
 require('dotenv').config();
 
 const app = express();
@@ -11,39 +11,22 @@ app.use(cookieParser());
 
 
 //creazione token con rotta post per mimare l'invio di un form
-app.get('/login',(req,res)=>{
-    const payload = { id: 1, isLogged: true}
-    
-    const cookieSetting = {
-        expires : new Date(Date.now() +100000),
-        httpOnly: true,
-        secure: false //perchè siamo in sviluppo
-    }
-    //vogliamo firmare il token non più con la chiave segreta ma con la chiave privata
-    const prv_key = fs.readFileSync('rsa.private')
-    const token= jwt.sign(payload, prv_key,options);
-    //memorizzo il token in un cookie
-    res.cookie('token', token,cookieSetting).send();
+app.get('/login',signToken,(req,res)=>{
+    res.send();
 })
 
 //se dopo aver fatto richiesta alla rotta login vado in questa rotta troverò il token
 // il client cioè ci sta inviando il token 
-app.get('/user/profile',checkAuth,(req,res)=>{
+app.get('/user/profile',verifyToken,(req,res)=>{
    res.send('sei autenticato!');
 })
 
-app.get('/user/message',checkAuth,(req,res)=>{
+app.get('/user/message',verifyToken,(req,res)=>{
    res.send('sei autenticato!');
 })
 
-app.get('/logout',(req,res)=>{
-   //per eliminare il cookie token dal client inviamo al client il cookie con scadenza nel passato
-    const cookieSetting = {
-        expires : new Date(0),
-        httpOnly: true,
-        secure: false //perchè siamo in sviluppo
-    }
-   res.cookie('token', '', cookieSetting).send('logout effettuato');
+app.get('/logout',deleteToken,(req,res)=>{
+   res.send('logout effettuato')
 })
 
 app.listen(3000, ()=> console.log('Server in ascolto sulla porta 3000'));
--- middleware/user-auth.js
+++ middleware/user-auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
-module.exports = (req,res,next) => {
+function verifyToken (req,res,next) {
      //per accedere al cookie inviato dal client uso il pacchetto cookie parser
     const token = req.cookies.token;
     //utilizzo l'oggetto options sia per la firma che per la verifica del token 
@@ -19,3 +19,36 @@ module.exports = (req,res,next) => {
     }
     // console.log(payload);
 }
+
+function deleteToken(req,res,next) {
+    //per eliminare il cookie token dal client inviamo al client il cookie con scadenza nel passato
+    const cookieSetting = {
+        expires : new Date(0),
+        httpOnly: true,
+        secure: false //perchè siamo in sviluppo
+    }
+   res.cookie('token', '', cookieSetting).send('logout effettuato');
+   next();
+}
+
+function signToken (req,res,next){
+const payload = { id: 1, isLogged: true}
+
+const cookieSetting = {
+    expires : new Date(Date.now() +100000),
+    httpOnly: true,
+    secure: false //perchè siamo in sviluppo
+}
+//vogliamo firmare il token non più con la chiave segreta ma con la chiave privata
+const prv_key = fs.readFileSync('rsa.private')
+const token= jwt.sign(payload, prv_key,options);
+//memorizzo il token in un cookie
+res.cookie('token', token,cookieSetting);
+next();
+}
+
+    module.exports = {
+        verifyToken,
+        signToken,
+        deleteToken
+    }
